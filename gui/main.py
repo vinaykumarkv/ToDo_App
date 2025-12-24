7894c5738f108b2b55c85456eaa6b0349336e418
Vinay Kumar K V <kvvk.win@gmail.com>
2025-12-24 20:58:46 +0000

    GUI App complete

--- gui/main.py
+++ gui/main.py
@@ -10,8 +10,15 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=get_todos(),key="todos",
                       enable_events=True, size=(45,10))
 edit_button = sg.Button("Edit")
+mark_button = sg.Button("Complete")
+
+layout = [[label],
+          [input_box, add_button],
+          [list_box, edit_button],
+          [mark_button]]
+
 window = sg.Window("My To Do App",
-                       layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                       layout=layout,
                        font=("Helvetica", 12))
 while True:
     event, values = window.read()
@@ -32,6 +39,12 @@ while True:
             window["todos"].Update(values=todos)
         case "todos":
             window["todo"].update(value=values['todos'][0])
+        case "Complete":
+            todos = functions.get_todos()
+            todo_complete = values["todos"][0]
+            todos.remove(todo_complete)
+            write_todos(todos)
+            window["todos"].Update(values=todos)
         case sg.WIN_CLOSED:
             break
 

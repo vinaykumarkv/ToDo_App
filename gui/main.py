0f0a4c1c28153647ebffd5d86403f2cc6dfeb758
Vinay Kumar K V <kvvk.win@gmail.com>
2025-12-24 19:16:52 +0000

    built add edit options

--- gui/main.py
+++ gui/main.py
@@ -1,15 +1,18 @@
 import functions
 import FreeSimpleGUI as sg
 
-from ToDo_App.gui.functions import write_todos
+from ToDo_App.gui.functions import write_todos, get_todos
 
 label = sg.Text(
     "To Do Task" )
 input_box = sg.InputText(tooltip="Enter To DO item",key="todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=get_todos(),key="todos",
+                      enable_events=True, size=(45,10))
+edit_button = sg.Button("Edit")
 window = sg.Window("My To Do App",
-                   layout=[[label],[input_box,add_button]],
-                   font=("Helvetica", 12))
+                       layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                       font=("Helvetica", 12))
 while True:
     event, values = window.read()
     print(event, values)
@@ -19,6 +22,16 @@ while True:
             new_todo = values["todo"]
             todos.append(new_todo+"\n")
             write_todos(todos)
+            window["todos"].Update(values=todos)
+        case "Edit":
+            todos = functions.get_todos()
+            todo_edit = values["todos"][0]
+            edit_todo = f"{values["todo"]}\n"
+            todos[todos.index(todo_edit)]=edit_todo
+            write_todos(todos)
+            window["todos"].Update(values=todos)
+        case "todos":
+            window["todo"].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
             break
 
